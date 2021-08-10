41bd5dc9221d86b4a7504c9944a11c43916bcd75
syedfaizz <syedfaizz45@gmail.com>
2021-08-10 17:57:54 +0530

    [faisal] Add: uc3-merge-sort

--- Algorithm.cs
+++ Algorithm.cs
@@ -4,32 +4,61 @@ using System.Text;
 
 namespace Algorithm
 {
-    class Algorihm<T> where T : IComparable
-    {
-        public T[] BubbleSort(T[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                T temp;
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i].CompareTo(arr[j]) > 0)
-                    {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                }
-            }
-            return arr;
-        }
-        public void Display(T[] arr)
-        {
-            foreach (T sort in arr)
-            {
-                Console.Write(sort + " ");
-            }
-            Console.WriteLine();
-        }
-    }
+	class Algorithm<T> where T : IComparable
+	{
+		public void MergeSort(T[] arr, int start, int end)
+		{
+			if (arr.Length == 1)
+			{
+				Console.WriteLine("Element is already sorted \n" + arr);
+			}
+			if (start < end)
+			{
+				int mid = (start + end) / 2;
+				Merge_Sort(arr, start, mid);
+				Merge_Sort(arr, mid + 1, end);
+				Merge(arr, start, mid, end);
+			}
+		}
+		public void Merge(T[] arr, int start, int mid, int end)
+		{
+			T[] temp = new T[end - start + 1];
+			int i = start, j = mid + 1, k = 0;
+			while (i <= mid && j <= end)
+			{
+				if (arr[i].CompareTo(arr[j]) <= 0)
+				{
+					temp[k] = arr[i];
+					k += 1; i += 1;
+				}
+				else
+				{
+					temp[k] = arr[j];
+					k += 1; j += 1;
+				}
+			}
+			while (i <= mid)
+			{
+				temp[k] = arr[i];
+				k += 1; i += 1;
+			}
+			while (j <= end)
+			{
+				temp[k] = arr[j];
+				k += 1; j += 1;
+			}
+			for (i = start; i <= end; i += 1)
+			{
+				arr[i] = temp[i - start];
+			}
+		}
+		public void Display(T[] arr)
+		{
+			foreach (T sort in arr)
+			{
+				Console.Write(sort + " ");
+			}
+			Console.WriteLine();
+		}
+	}
 }
