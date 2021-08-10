7415bce2310726d87eedb990ecf867b6e2c30ffc
syedfaizz <syedfaizz45@gmail.com>
2021-08-10 17:48:39 +0530

    [faisal] Add: uc1-binary-search

---
+++ Algorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class Algorithm<T> where T : IComparable
+    {
+        T[] array;
+        public Algorithm(T[] array)
+        {
+            this.array = array;
+            Array.Sort(this.array);
+        }
+       
+        public int BinarySearch(T[] array, T element)
+        {
+            int low = 0, high = array.Length - 1;
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int key = element.CompareTo(array[mid]);
+                if (key == 0)
+                    return mid;
+                if (key > 0)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+            return -1;
+        }
+    }
+}
