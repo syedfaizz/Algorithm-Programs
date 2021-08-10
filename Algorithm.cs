a8c561e6d3cd92ef329425af518fbd3884638b81
syedfaizz <syedfaizz45@gmail.com>
2021-08-10 18:03:45 +0530

    [faisal] Add: uc4-insertion-sort

--- Algorithm.cs
+++ Algorithm.cs
@@ -4,61 +4,30 @@ using System.Text;
 
 namespace Algorithm
 {
-	class Algorithm<T> where T : IComparable
-	{
-		public void MergeSort(T[] arr, int start, int end)
-		{
-			if (arr.Length == 1)
-			{
-				Console.WriteLine("Element is already sorted \n" + arr);
-			}
-			if (start < end)
-			{
-				int mid = (start + end) / 2;
-				Merge_Sort(arr, start, mid);
-				Merge_Sort(arr, mid + 1, end);
-				Merge(arr, start, mid, end);
-			}
-		}
-		public void Merge(T[] arr, int start, int mid, int end)
-		{
-			T[] temp = new T[end - start + 1];
-			int i = start, j = mid + 1, k = 0;
-			while (i <= mid && j <= end)
-			{
-				if (arr[i].CompareTo(arr[j]) <= 0)
-				{
-					temp[k] = arr[i];
-					k += 1; i += 1;
-				}
-				else
-				{
-					temp[k] = arr[j];
-					k += 1; j += 1;
-				}
-			}
-			while (i <= mid)
-			{
-				temp[k] = arr[i];
-				k += 1; i += 1;
-			}
-			while (j <= end)
-			{
-				temp[k] = arr[j];
-				k += 1; j += 1;
-			}
-			for (i = start; i <= end; i += 1)
-			{
-				arr[i] = temp[i - start];
-			}
-		}
-		public void Display(T[] arr)
-		{
-			foreach (T sort in arr)
-			{
-				Console.Write(sort + " ");
-			}
-			Console.WriteLine();
-		}
-	}
+    class Algorithm<T> where T : IComparable
+    {
+        public void InsertionSort(T[] arr)
+        {
+            T max;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                max = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j].CompareTo(max) > 0)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = max;
+            }
+        }
+        public void Display(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
 }
