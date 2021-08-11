8bcf65d5e8c29c17712ac267da20f24829999e55
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-08-11 08:24:37 +0530

    Update Algorithm.cs

--- Algorithm.cs
+++ Algorithm.cs
@@ -2,46 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Algorithm
-{
-    class Algorithm
-    {
-        static readonly ArrayList palindromeList = new ArrayList();
-        const int LIMIT = 1000;
-        public static void PrintPrime()
-        {
-            bool temp;
-            for (int i = 11; i <= LIMIT; i++)
-            {
-                temp = false;
-                for (int j = 11; j <= i / 2; j++)
-                {
-                    if (i % j == 0)
-                    {
-                        temp = true;
-                        break;
-                    }
-                }
-                if (temp == false)
-                    PrimePalindrome(i);
-            }
-            Console.WriteLine();
-        }
-        public static void PrimePalindrome(int num)
-        {
-            if (PalindromePrime.IsPalindrome(num) == true)
-            {
-                palindromeList.Add(num);
-            }
-        }
-        public static void PrintPalindrome()
-        {
-            Console.WriteLine("Palindrome-Prime in Range");
-            foreach (int prime in palindromeList)
-            {
-                Console.Write(prime + " ");
-            }
-            Console.WriteLine();
-        }
-    }
-}
+   public class Algorithm
+   {  
+     public static void Main(string[] args)  
+      {  
+          int n,r,sum=0,temp;    
+          Console.Write("Enter the Number: ");   
+          n = int.Parse(Console.ReadLine());  
+          temp=n;      
+          while(n>0)      
+          {      
+           r=n%10;      
+           sum=(sum*10)+r;      
+           n=n/10;      
+          }      
+          if(temp==sum)      
+           Console.Write("Number is Palindrome.");      
+          else      
+           Console.Write("Number is not Palindrome");     
+    }  
+  }
