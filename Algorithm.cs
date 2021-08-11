99ba9e1f5611ee48371c00383bd3cef0b155ae35
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-08-11 08:44:34 +0530

    Update Algorithm.cs

--- Algorithm.cs
+++ Algorithm.cs
@@ -2,32 +2,31 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Algorithm
-{
-    class Algorithm<T> where T : IComparable
-    {
-        public void InsertionSort(T[] arr)
-        {
-            T max;
-            for (int i = 1; i < arr.Length; i++)
-            {
-                max = arr[i];
-                int j = i - 1;
-                while (j >= 0 && arr[j].CompareTo(max) > 0)
-                {
-                    arr[j + 1] = arr[j];
-                    j--;
-                }
-                arr[j + 1] = max;
+namespace Algorithm {
+   class Algorithm {
+      static void Main(string[] args) {
+         int[] arr = new int[10] { 23, 9, 85, 12, 99, 34, 60, 15, 100, 1 };
+         int n = 10, i, j, val, flag;
+         Console.WriteLine("Insertion Sort");
+         Console.Write("Initial array is: ");
+         for (i = 0; i < n; i++) {
+            Console.Write(arr[i] + " ");
+         }
+         for (i = 1; i < n; i++) {
+            val = arr[i];
+            flag = 0;
+            for (j = i - 1; j >= 0 && flag != 1; ) {
+               if (val < arr[j]) {
+                  arr[j + 1] = arr[j];
+                  j--;
+                  arr[j + 1] = val;
+               }
+               else flag = 1;
             }
-        }
-        public void Display(T[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
-            Console.WriteLine();
-        }
-    }
-}
+         }
+         Console.Write("\nSorted Array is: ");
+         for (i = 0; i < n; i++) {
+            Console.Write(arr[i] + " ");
+         }
+      }
+  
