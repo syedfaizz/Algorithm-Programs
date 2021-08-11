87fd35e60446962897262b341453e64492a9e89e
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-08-11 23:43:55 +0530

    Update Algorithm.cs

--- Algorithm.cs
+++ Algorithm.cs
@@ -1,32 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Algorithm
+namespace Algorithm 
 {
-    class Algorithm<T> where T : IComparable
-    {
-        T[] array;
-        public Algorithm(T[] array)
-        {
-            this.array = array;
-            Array.Sort(this.array);
-        }
-       
-        public int BinarySearch(T[] array, T element)
-        {
-            int low = 0, high = array.Length - 1;
-            while (low <= high)
-            {
-                int mid = (low + high) / 2;
-                int key = element.CompareTo(array[mid]);
-                if (key == 0)
-                    return mid;
-                if (key > 0)
-                    low = mid + 1;
-                else
-                    high = mid - 1;
-            }
-            return -1;
-        }
-    }
+   class Algorithm 
+   {
+public static object BinarySearchDisplay(int[] arr, int key) {
+   int minNum = 0;
+   int maxNum = arr.Length - 1;
+
+   while (minNum <=maxNum){
+      int mid = (minNum + maxNum) / 2;
+      if (key == arr[mid]) {
+         return ++mid;
+      } else if (key < arr[mid]) {
+         max = mid - 1;
+      }else {
+         min = mid + 1;
+      }
+   }
+   return "None";
+}
