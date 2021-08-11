da34dfa12e451d38730dacfe282a825891d3f275
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-08-11 08:56:03 +0530

    Update Algorithm.cs

--- Algorithm.cs
+++ Algorithm.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Algorithm {
-   class Algorithm {
-      static void Main(string[] args) {
+namespace Algorithm 
+{
+   class Algorithm 
+   {
+      static void Main(string[] args) 
+      {
          int[] arr = new int[10] { 23, 9, 85, 12, 99, 34, 60, 15, 100, 1 };
          int n = 10, i, j, val, flag;
          Console.WriteLine("Insertion Sort");
@@ -12,11 +15,14 @@ namespace Algorithm {
          for (i = 0; i < n; i++) {
             Console.Write(arr[i] + " ");
          }
-         for (i = 1; i < n; i++) {
+         for (i = 1; i < n; i++) 
+         {
             val = arr[i];
             flag = 0;
-            for (j = i - 1; j >= 0 && flag != 1; ) {
-               if (val < arr[j]) {
+            for (j = i - 1; j >= 0 && flag != 1; ) 
+            {
+               if (val < arr[j]) 
+               {
                   arr[j + 1] = arr[j];
                   j--;
                   arr[j + 1] = val;
@@ -29,4 +35,4 @@ namespace Algorithm {
             Console.Write(arr[i] + " ");
          }
       }
-  
+   }
