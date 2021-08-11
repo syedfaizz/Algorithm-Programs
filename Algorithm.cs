ae73677a5d215e6096707b9d7d9fa732e695b9a8
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-08-11 23:58:17 +0530

    Update Algorithm.cs

--- Algorithm.cs
+++ Algorithm.cs
@@ -6,19 +6,28 @@ namespace Algorithm
 {
    class Algorithm 
    {
-public static object BinarySearchDisplay(int[] arr, int key) {
+public static object BinarySearchDisplay(int[] arr, int key) 
+{
    int minNum = 0;
    int maxNum = arr.Length - 1;
 
-   while (minNum <=maxNum){
+   while (minNum <=maxNum)
+   {
       int mid = (minNum + maxNum) / 2;
-      if (key == arr[mid]) {
+      if (key == arr[mid]) 
+      {
          return ++mid;
-      } else if (key < arr[mid]) {
+      }
+      else if (key < arr[mid])
+      {
          max = mid - 1;
-      }else {
+      }
+      else 
+      {
          min = mid + 1;
       }
    }
    return "None";
+     }
+   }
 }
