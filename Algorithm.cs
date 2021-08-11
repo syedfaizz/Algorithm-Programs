b659087e2630f46d91a09266d73d00508331499c
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-08-11 09:51:18 +0530

    Update Algorithm.cs

--- Algorithm.cs
+++ Algorithm.cs
@@ -2,63 +2,81 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Algorithm
+namespace Algorithm 
 {
-	class Algorithm<T> where T : IComparable
-	{
-		public void MergeSort(T[] arr, int start, int end)
+   class Algorithm 
+   {
+      static public void merge(int[] arr, int p, int q, int r) 
+      {
+         int i, j, k;
+         int n1 = q - p + 1;
+         int n2 = r - q;
+         int[] L = new int[n1];
+         int[] R = new int[n2];
+         for (i = 0; i < n1; i++) 
+	 {
+            L[i] = arr[p + i];
+         }
+         for (j = 0; j < n2; j++) 
+	 {
+            R[j] = arr[q + 1 + j];
+         }
+         i = 0;
+         j = 0;
+         k = p;
+         while (i < n1 && j < n2) 
+	 {
+            if (L[i] <= R[j]) 
+	    {
+               arr[k] = L[i];
+               i++;
+            }
+		else
 		{
-			if (arr.Length == 1)
-			{
-				Console.WriteLine("Element is already sorted \n" + arr);
-			}
-			if (start < end)
-			{
-				int mid = (start + end) / 2;
-				Merge_Sort(arr, start, mid);
-				Merge_Sort(arr, mid + 1, end);
-				Merge(arr, start, mid, end);
-			}
-		}
-		public void Merge(T[] arr, int start, int mid, int end)
-		{
-			T[] temp = new T[end - start + 1];
-			int i = start, j = mid + 1, k = 0;
-			while (i <= mid && j <= end)
-			{
-				if (arr[i].CompareTo(arr[j]) <= 0)
-				{
-					temp[k] = arr[i];
-					k += 1; i += 1;
-				}
-				else
-				{
-					temp[k] = arr[j];
-					k += 1; j += 1;
-				}
-			}
-			while (i <= mid)
-			{
-				temp[k] = arr[i];
-				k += 1; i += 1;
-			}
-			while (j <= end)
-			{
-				temp[k] = arr[j];
-				k += 1; j += 1;
-			}
-			for (i = start; i <= end; i += 1)
-			{
-				arr[i] = temp[i - start];
-			}
-		}
-		public void Display(T[] arr)
-		{
-			foreach (T sort in arr)
-			{
-				Console.Write(sort + " ");
-			}
-			Console.WriteLine();
-		}
-	}
+               arr[k] = R[j];
+               j++;
+            }
+            k++;
+         }
+         while (i < n1) 
+	 {
+            arr[k] = L[i];
+            i++;
+            k++;
+         }
+         while (j < n2) 
+	 {
+            arr[k] = R[j];
+            j++;
+            k++;
+         }
+      }
+      static public void mergeSort(int[] arr, int p, int r) 
+      {
+         if (p < r) 
+	 {
+            int q = (p + r) / 2;
+            mergeSort(arr, p, q);
+            mergeSort(arr, q + 1, r);
+            merge(arr, p, q, r);
+         }
+      }
+      static void Main(string[] args) 
+      {
+         int[] arr = {76, 89, 23, 1, 55, 78, 99, 12, 65, 100};
+         int n = 10, i;
+         Console.WriteLine("Merge Sort");
+         Console.Write("Initial array is: ");
+         for (i = 0; i < n; i++) 
+	 {
+            Console.Write(arr[i] + " ");
+         }
+         mergeSort(arr, 0, n-1);
+         Console.Write("\nSorted Array is: ");
+         for (i = 0; i < n; i++) 
+	 {
+            Console.Write(arr[i] + " ");
+         }
+      }
+   }
 }
